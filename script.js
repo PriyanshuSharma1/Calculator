deb7e579b80f8a73754a842d0be7ae05cac1f36e
Priyanshu <psneb420@gmail.com>
2023-09-20 17:58:08 +0545

    Enhancing the visual appearance of the calculator

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-let string ="";
+let equationString ="";
+let resultString="";
 
 let buttons= document.querySelectorAll(".button");
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
         if(e.target.innerHTML == "="){
-            string=eval(string);
-            document.querySelector("input").value=string;
+            //evaluate the equation
+            resultString=eval(equationString);
+            //display the equation and result
+            document.getElementById("equation").value=equationString;
+            document.getElementById("result").value=resultString;
+            //store result for further calculations
+            equationString=resultString;
         }
         else if(e.target.innerHTML== "AC"){
-            string="";
-            document.querySelector("input").value=string;
+            equationString="";
+            resultString="";
+            document.getElementById("equation").value=equationString;
+            document.getElementById("result").value=resultString;
 
         }
         else{
-            console.log(e.target);
-            string=string+e.target.innerHTML;
-            document.querySelector("input").value=string;
+            equationString=equationString+e.target.innerHTML;
+            document.getElementById("equation").value=equationString;
         }
+        // Change the button color and add an effect when clicked
+        e.target.style.backgroundColor = "blue";
+        e.target.style.color="white";  // Change to any desired color
+        setTimeout(() => {
+            e.target.style.backgroundColor = "";
+            e.target.style.color="";   // Reset the color after a delay
+        }, 200);
+        
     })
 })
